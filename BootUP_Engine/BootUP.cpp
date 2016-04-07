79e285911ebea8c22299ad4a682f6c0d0cd5a676
Charles Spivey <caspivey1@gmail.com>
2016-04-07 10:29:34 -0400

    Added Utility Header and moved frame rate display code to it.

--- BootUP_Engine/BootUP.cpp
+++ BootUP_Engine/BootUP.cpp
@@ -10,6 +10,7 @@
 #include "PhysicsSystem.hpp"
 #include "Camera.hpp"
 #include "ShaderProgram.hpp"
+#include "Utility.hpp"
 
 std::string windowTitle = "BootUp Engine";
 
@@ -18,68 +19,9 @@ double currentFrameTime = 0.0;
 double lastFrameTime = 0.0;
 double maxFramerate = 60.0;
 
-double calcFPS(GLFWwindow* window, double theTimeInterval, std::string theWindowTitle)
-{
-	// Static values which only get initialised the first time the function runs
-	static double t0Value = glfwGetTime();	// Set the initial time to now
-	static int    fpsFrameCount = 0;		// Set the initial FPS frame count to 0
-	static double fps = 0.0;				// Set the initial FPS value to 0.0
-
-	// Get the current time in seconds since the program started (non-static, so executed every time)
-	double currentTime = glfwGetTime();
-
-	// Ensure the time interval between FPS checks is sane (low cap = 0.1s, high-cap = 10.0s)
-	// Negative numbers are invalid, 10 fps checks per second at most, 1 every 10 secs at least.
-	if (theTimeInterval < 0.1)
-	{
-		theTimeInterval = 0.1;
-	}
-	if (theTimeInterval > 10.0)
-	{
-		theTimeInterval = 10.0;
-	}
-
-	// Calculate and display the FPS every specified time interval
-	if ((currentTime - t0Value) > theTimeInterval)
-	{
-		// Calculate the FPS as the number of frames divided by the interval in seconds
-		fps = (double)fpsFrameCount / (currentTime - t0Value);
-
-		// If the user specified a window title to append the FPS value to...
-		if (theWindowTitle != "NONE")
-		{
-			// Convert the fps value into a string using an output stringstream
-			std::ostringstream stream;
-			stream << fps;
-			std::string fpsString = stream.str();
-
-			// Append the FPS value to the window title details
-			theWindowTitle += " | FPS: " + fpsString;
-
-			// Convert the new window title to a c_str and set it
-			const char* pszConstString = theWindowTitle.c_str();
-			glfwSetWindowTitle(window, pszConstString);
-		}
-		else // If the user didn't specify a window to append the FPS to then output the FPS to the console
-		{
-			std::cout << "FPS: " << fps << std::endl;
-		}
-
-		// Reset the FPS frame counter and set the initial time to be now
-		fpsFrameCount = 0;
-		t0Value = glfwGetTime();
-	}
-	else // FPS calculation time interval hasn't elapsed yet? Simply increment the FPS frame counter
-	{
-		fpsFrameCount++;
-	}
-
-	// Return the current FPS - doesn't have to be used if you don't want it!
-	return fps;
-}
-
 int main()
 {
+	Framerate fps_viewer;
 	GLFWwindow* window;
 
 	//check if glfw failed to initialize
@@ -137,7 +79,7 @@ int main()
 		{
 			
 			lastFrameTime = currentFrameTime;
-			calcFPS(window, 1.0, windowTitle);
+			fps_viewer.calculateFPS(window, 1.0, windowTitle);
 
 			//clear the depth and color buffers
 			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
